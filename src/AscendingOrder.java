8432145e2498d0a6fb15002eef1a1ab362fb05ce
OlegAngelo <naubalagtas@gmail.com>
2019-12-16 14:32:39 +0800

    finish output

--- src/AscendingOrder.java
+++ src/AscendingOrder.java
@@ -1,2 +1,41 @@
+import java.util.*;
+
 public class AscendingOrder {
+    public static void sortArrayInAscendingOrder(int arrayOfIntegers[], int maxLengthOfTheArray) {
+
+        // make a collection to store positive integers
+        Vector<Integer> storePositiveNumbers = new Vector<>();
+
+        for (int index = 0; index < maxLengthOfTheArray; index++) {
+            if (arrayOfIntegers[index] >= 0)
+                storePositiveNumbers.add(arrayOfIntegers[index]);
+        }
+
+        // sort the stored integers in ascending order(positive numbers)
+        Collections.sort(storePositiveNumbers);
+
+        // if current element is positive, replace it with the sorted element from the stored numbers
+        int storedInteger = 0;
+        for (int index = 0; index < maxLengthOfTheArray; index++) {
+
+            if (arrayOfIntegers[index] >= 0)
+            {
+                arrayOfIntegers[index] = storePositiveNumbers.get(storedInteger);
+                storedInteger++;
+            }
+        }
+
+        // print the new sorted array
+        for (int index = 0; index < maxLengthOfTheArray; index++)
+            System.out.print(arrayOfIntegers[index] + ", ");
+    }
+
+    public static void main(String[] args){
+
+        int[] arrayOfIntegers = {7, 2, 8, -1, 4, 5, -3, -6};
+        int maxLengthOfTheArray = arrayOfIntegers.length;
+
+        sortArrayInAscendingOrder(arrayOfIntegers, maxLengthOfTheArray);
+    }
+
 }
